b62c335877a0493f5db4e3c8c1aef106a069cb37
Seth93 <seth.iitr@gmail.com>
2016-09-21 13:48:09 +0530

    Update acatesting.go

--- examples/acatesting.go
+++ examples/acatesting.go
@@ -20,16 +20,16 @@ type ServicesChaincode struct {
 }
 
 func (t *ServicesChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
- 	myLogger.Debug("Init Code")
-	myLogger.Debugf("Init Code 1")
-	myLogger.Info("Init Code 2")
-	myLogger.Notice("Init Code 3")
-        myLogger.Warning("Init Code 4")
-        myLogger.Error("Init Code 5")
-        myLogger.Critical("Init Code 6")
+ //	myLogger.Debug("Init Code")
+	// myLogger.Debugf("Init Code 1")
+	// myLogger.Info("Init Code 2")
+	// myLogger.Notice("Init Code 3")
+ //       myLogger.Warning("Init Code 4")
+ //       myLogger.Error("Init Code 5")
+ //       myLogger.Critical("Init Code 6")
 
-	err := stub.PutState("counter", []byte("0"))
-	return nil, err
+	err := stub.PutState("role", []byte("0"))
+	return nil, nil
 }
 
 
@@ -38,60 +38,40 @@ func (t *ServicesChaincode) Invoke(stub *shim.ChaincodeStub, function string, ar
 	 if function != "increment" {
 	 	return nil, errors.New("Invalid invoke function name. Expecting \"increment\"")
 	 }
-	val, err := stub.ReadCertAttribute("position")
-	fmt.Printf("Position => %v error %v \n", string(val), err)
-	isOk, err := stub.VerifyAttribute("position", []byte("Software Engineer")) // Here the ABAC API is called to verify the attribute, just if the value is verified the counter will be incremented.
-	if err != nil {
-		return nil, err
-	}
-	if isOk {
-		counter, err := stub.GetState("counter")
-		if err != nil {
-			return nil, err
-		}
-		var cInt int
-		cInt, err = strconv.Atoi(string(counter))
-		if err != nil {
-			return nil, err
-		}
-		cInt = cInt + 1
-		counter = []byte(strconv.Itoa(cInt))
-		stub.PutState("counter", counter)
-	}
+	val, _ := stub.ReadCertAttribute("role")
+	fmt.Printf("role => %v \n", string(val))
 	return val, nil
 }
 
 /*
- 		Get Customer record by customer id or PAN number
+ 	
 */
 func (t *ServicesChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if function == "read" {
 		return read(stub, args)
-	}else {
-	 	return readAttr(stub, args)
-	}
+	}else 
 	return nil, errors.New("Invalid query function name. Expecting \"read\"")
 }
 
-func readAttr(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	attrVal1, err := stub.ReadCertAttribute("position")
-	isPresent, err := stub.VerifyAttribute("position", []byte("Software Engineer")) // Here the ABAC API is called to verify the attribute, just if the value is verified the counter will be incremented.
-	if err != nil {
-		return nil, err
-	}
-	jsonResp := "{ " +
-					"Attribute Name  01: "+ string(attrVal1) +
-					"Attribute Value  01 : "+ strconv.FormatBool(isPresent) +
+// func readAttr(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+// 	attrVal1, err := stub.ReadCertAttribute("position")
+// 	isPresent, err := stub.VerifyAttribute("position", []byte("Software Engineer")) // Here the ABAC API is called to verify the attribute, just if the value is verified the counter will be incremented.
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	jsonResp := "{ " +
+// 					"Attribute Name  01: "+ string(attrVal1) +
+// 					"Attribute Value  01 : "+ strconv.FormatBool(isPresent) +
 
-				 "}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
+// 				 "}"
+// 	fmt.Printf("Query Response:%s\n", jsonResp)
 
-	bytes, err := json.Marshal(jsonResp)
-	if err != nil {
-		return nil, errors.New("Error converting kyc record")
-	}
-	return bytes, nil
-}
+// 	bytes, err := json.Marshal(jsonResp)
+// 	if err != nil {
+// 		return nil, errors.New("Error converting kyc record")
+// 	}
+// 	return bytes, nil
+// }
 
 
 func read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
@@ -104,7 +84,7 @@ func read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	// }
 
 	// Get the state from the ledger
-	Avalbytes, err := stub.GetState("counter")
+	Avalbytes, err := stub.GetState("role")
 	if err != nil {
 		jsonResp := "{\"Error\":\"Failed to get state for counter\"}"
 		return nil, errors.New(jsonResp)
